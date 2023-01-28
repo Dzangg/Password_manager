7964a1e88e22962e10c62c2c548fec229364568c
Bartlomiej Bielecki <bartlomiej.bielecki36@gmail.com>
2023-01-28 22:45:27 +0100

    First view design

--- main.py
+++ main.py
@@ -1,93 +1,145 @@
 import sys
-
+import os
 from PyQt5 import QtSvg
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QCursor, QIcon
-from PyQt5.QtWidgets import (
-    QApplication,
-    QWidget,
-    QLabel,
-    QPushButton,
-    QMainWindow,
-    QVBoxLayout,
-    QLineEdit,
-    QMenu,
-    QMenuBar,
-    QToolBar,
-
-)
+from PyQt5.QtWidgets import *
 
 WIN_SIZE = 500
+with open("resources/css/styles.css", 'r') as f:
+    css = f.read()
 
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Password Manager")
         self.setFixedSize(WIN_SIZE, WIN_SIZE)
+        self.setStyleSheet(" background-color: #fff; ")
 
-        self.centralwidget = QWidget(self)
-        self.centralwidget.resize(WIN_SIZE, WIN_SIZE)
+        outerLayout = QVBoxLayout()
+        topLayout = QVBoxLayout()
+        middleLayout = QVBoxLayout()
 
-        self._createStyleWindow()
-        self._createMenuBar()
-        # self._createToolBars()
+        outerLayout.setAlignment(Qt.AlignCenter)
+        middleLayout.setAlignment(Qt.AlignCenter)
+        middleLayout.setSpacing(30)
 
-    def _createStyleWindow(self):
-        # Menu
-        self.setContextMenuPolicy(Qt.CustomContextMenu)
-        self.customContextMenuRequested.connect(self.closeMenu)
+        self._createLabel()
+        self._createButtons()
 
-        # Initial
-        self.setWindowFlag(Qt.FramelessWindowHint)
-        self.setAttribute(Qt.WA_TranslucentBackground)
-        # self.setWindowOpacity(0.9)
+        centralwidget = QWidget(self)
+        self.setCentralWidget(centralwidget)
 
-        radius = 10
-        r, g, b = 235, 231, 230
-        self.centralwidget.setStyleSheet(
-            """
-            background:rgb({0},{1},{2});
-            border-radius:{3}px;
-            """.format(r, g, b, radius)
-        )
+        topLayout.addWidget(self.label)
+        middleLayout.addWidget(self.loginBtn)
+        middleLayout.addWidget(self.signupBtn)
 
-    def _createMenuBar(self):
-        menuBar = self.menuBar()
-        exitMenu = QMenu("&Exit", self)
-        minimizeMenu = QMenu("&Minimize", self)
-        menuBar.addMenu(exitMenu)
-        menuBar.addMenu(minimizeMenu)
-        menuBar.setLayoutDirection(Qt.RightToLeft)
-        self.setMenuBar(menuBar)
+        outerLayout.addLayout(topLayout)
+        outerLayout.addLayout(middleLayout)
+        centralwidget.setLayout(outerLayout)
 
-    def closeMenu(self, pos):
-        menu = QMenu()
+    def _createLabel(self):
+        self.label = QLabel("Password Manager")
+        self.label.setObjectName("mainLabel")
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setFixedSize(300, 200)
+        self.label.setStyleSheet(css)
+        return self.label
 
-        exit_option = menu.addAction('Exit')
+    def _createButtons(self):
+        self.loginBtn = QPushButton("Log In")
+        self.loginBtn.setObjectName("loginBtn")
+        self.loginBtn.setFixedSize(150, 50)
+        self.signupBtn = QPushButton("Sign up")
+        self.signupBtn.setObjectName("signupBtn")
+        self.signupBtn.setFixedSize(150, 50)
+        self.signupBtn.setCursor(QCursor(Qt.PointingHandCursor))
+        self.loginBtn.setCursor(QCursor(Qt.PointingHandCursor))
+        self.signupBtn.clicked.connect(self.on_button_signUp)
+        self.loginBtn.clicked.connect(self.on_button_logIn)
 
-        exit_option.triggered.connect(lambda: exit())
+        self.loginBtn.setStyleSheet(css)
+        self.signupBtn.setStyleSheet(css)
 
-        menu.exec_(self.mapToGlobal(pos))
+    def on_button_logIn(self):
+        formLogIn = form("logIn")
 
-    def mousePressEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            self.moveFlag = True
-            self.movePosition = event.globalPos() - self.pos()
-            self.setCursor(QCursor(Qt.ClosedHandCursor))
-            event.accept()
+    def on_button_signUp(self):
+        self.setCentralWidget(signUp())
 
-    def mouseMoveEvent(self, event):
-        if Qt.LeftButton and self.moveFlag:
-            self.move(event.globalPos() - self.movePosition)
-            event.accept()
 
-    def mouseReleaseEvent(self, QMouseEvent):
-        self.moveFlag = False
-        self.setCursor(Qt.ArrowCursor)
+class signUp(QWidget):
+    def __init__(self):
+        super().__init__()
+        # self.setModal(True)  # disable interaction with prev window until this one close
+        self.setFixedSize(350, 350)
+        self.main_window = Window()
+        self.appLabel = self.main_window.label
+
+        outerLayout = QVBoxLayout()
+        topLayout = QVBoxLayout()
+        formLayout = QVBoxLayout()
+        bottomLayout = QHBoxLayout()
+
+        outerLayout.setAlignment(Qt.AlignCenter)
+        formLayout.setAlignment(Qt.AlignCenter)
+        bottomLayout.setAlignment(Qt.AlignRight)
+
+        self._createForm()
+
+        topLayout.addWidget(self.appLabel)
+        formLayout.addWidget(self.labelName)
+        formLayout.addWidget(self.name)
+        formLayout.addWidget(self.labelPassword)
+        formLayout.addWidget(self.password)
+        bottomLayout.addWidget(self.acceptBtn)
+        bottomLayout.addWidget(self.cancelBtn)
+
+        outerLayout.addLayout(topLayout)
+        outerLayout.addLayout(formLayout)
+        outerLayout.addSpacing(100)
+        outerLayout.addLayout(bottomLayout)
+        self.setLayout(outerLayout)
+
+    def _createForm(self):
+        self.labelName = QLabel("Name:")
+        self.labelPassword = QLabel("Password:")
+        self.name = QLineEdit()
+        self.password = QLineEdit()
+        self.name.setFixedSize(200, 20)
+        self.password.setFixedSize(200, 20)
+        self.password.setEchoMode(QLineEdit.Password)
+
+        self.acceptBtn = QPushButton("Ok")
+        self.cancelBtn = QPushButton("Cancel")
+
+        self.acceptBtn.setFixedSize(100, 40)
+        self.cancelBtn.setFixedSize(100, 40)
+
+        self.acceptBtn.setObjectName("okButton")
+        self.cancelBtn.setObjectName("cancelButton")
+
+        self.acceptBtn.setStyleSheet(css)
+        self.cancelBtn.setStyleSheet(css)
+
+        self.acceptBtn.setCursor(QCursor(Qt.PointingHandCursor))
+        self.cancelBtn.setCursor(QCursor(Qt.PointingHandCursor))
+
+        self.acceptBtn.clicked.connect(self._submitForm)
+        self.cancelBtn.clicked.connect(self._cancelForm)
+
+    def _submitForm(self):
+        pass
+
+    def _cancelForm(self):
+        self.setCentralWidget(signUp())
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Window()
+
     window.show()
+
     sys.exit(app.exec_())
